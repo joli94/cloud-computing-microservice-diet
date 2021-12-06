3dc3e90c2c21219b2b41a7a790a5e3c453ae746d
joli94 <adrian.apostol.94@gmail.com>
2021-12-06 21:43:08 +0200

    first commit

---
+++ src/main/java/ro/unibuc/fmi/dietapp/microservice/diet/DietApplication.java
@@ -0,0 +1,13 @@
+package ro.unibuc.fmi.dietapp.microservice.diet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/unibuc/fmi/dietapp/microservice/diet/DietApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.unibuc.fmi.dietapp.microservice.diet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
