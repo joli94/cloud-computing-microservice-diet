2a50e58c0b6e0d6be5ec5f8cfb79ba55c1559358
joli94 <adrian.apostol.94@gmail.com>
2021-12-09 21:53:54 +0200

    Added Eureka client and Zipkin client

--- src/main/java/ro/unibuc/fmi/dietapp/microservice/diet/DietApplication.java
+++ src/main/java/ro/unibuc/fmi/dietapp/microservice/diet/DietApplication.java
@@ -2,8 +2,10 @@ package ro.unibuc.fmi.dietapp.microservice.diet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DietApplication {
 
     public static void main(String[] args) {
